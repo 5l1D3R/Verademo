2b96c05a9a27a949c8b29d66256c89476ac42345
Julian <jtotzekhallhuber@veracode.com>
2021-06-25 17:40:41 +0200

    commit

--- src/main/java/com/veracode/verademo/controller/ToolsController.java
+++ src/main/java/com/veracode/verademo/controller/ToolsController.java
@@ -54,6 +54,36 @@ public class ToolsController {
 			/* START BAD CODE */
 			if (System.getProperty("os.name").startsWith("Windows")) {
 				proc = Runtime.getRuntime().exec("cmd.exe /c ping " + host);
+			}
+			else {
+				proc = Runtime.getRuntime().exec("ping " + host);
+			}
+			/* END BAD CODE */
+
+
+			InputStreamReader isr = new InputStreamReader(proc.getInputStream());
+			BufferedReader br = new BufferedReader(isr);
+
+			String line;
+
+			while ((line = br.readLine()) != null) {
+				output += line + "\n";
+			}
+		}
+		catch (IOException ex) {
+			logger.error(ex);
+		}
+		return output;
+		
+	}
+
+	private String pong(String host)
+	{
+		String output = "";
+		Process proc;
+		try {
+			/* START BAD CODE */
+			if (System.getProperty("os.name").startsWith("Windows")) {
 				proc = Runtime.getRuntime().exec("cmd.exe /c ping " + host);
 			}
 			else {
@@ -76,9 +106,8 @@ public class ToolsController {
 		}
 		return output;
 		
-		
-		
 	}
+
 
 	private String fortune(String fortuneFile)
 	{
